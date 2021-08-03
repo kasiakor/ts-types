f30d91d89246cfa3767273d4c7642363116d0712
kasia.kordys <kasia.kordys@gmail.com>
2021-08-03 20:49:59 +0300

    use optional parameter with default value

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,10 @@
-function calcTax(amount, discount) {
-    return (amount * 1.2) - (discount || 0);
+function calcTax(amount, discount = 10) {
+    return (amount * 1.2) - discount;
 }
 let taxValue = calcTax(260, 0);
-console.log(`Tax value fgor 2 args is ${taxValue}`);
-//Expected 2 arguments, but got 1
+console.log(`Tax value for 2 args is ${taxValue}`);
+//Expected 2 arguments, but got 1 opt with default value
+// param with default value is optional
 taxValue = calcTax(100);
 console.log(`Tax value for 2 args including 1 optional is ${taxValue}`);
 //Expected 2 arguments, but got 3
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-function calcTax(amount, discount?) {
-    return (amount * 1.2) - (discount || 0);
+function calcTax(amount, discount = 10) {
+    return (amount * 1.2) - discount;
 }
 
 let taxValue = calcTax(260, 0);
-console.log(`Tax value fgor 2 args is ${taxValue}`);
+console.log(`Tax value for 2 args is ${taxValue}`);
 
-//Expected 2 arguments, but got 1
+//Expected 2 arguments, but got 1 opt with default value
+// param with default value is optional
 taxValue = calcTax(100);
 console.log(`Tax value for 2 args including 1 optional is ${taxValue}`);
 
