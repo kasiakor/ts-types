d0960f3adfb32475502411a45d30c9de7e0cb261
kasia.kordys <kasia.kordys@gmail.com>
2021-08-04 16:12:54 +0300

    use type annotations for function results

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,6 @@
 function calcTax(amount, discount = 10, ...extraFees) {
-    if (amount !== null) {
-        return (amount * 1.2) - discount +
-            extraFees.reduce((total, value) => total + value, 0);
-    }
-    else {
-        return undefined;
-    }
+    return (amount * 1.2) - discount +
+        extraFees.reduce((total, value) => total + value, 0);
 }
-//with null checks enabled  Argument of type 'null' is not assignable to parameter of type 'number'.
-let taxValue = calcTax(null, 0);
-console.log(`Tax value for 2 args including 1 null is ${taxValue}`);
+let taxValue = calcTax(100, 0);
+console.log(`Tax value is ${taxValue}`);
