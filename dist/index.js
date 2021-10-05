79d3c5f581ddd5bb2629d8207226332f520d0528
kasia.kordys <kasia.kordys@gmail.com>
2021-10-05 17:28:19 +0300

    use optional properties for irregular shapes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 let hat = { name: "Hat", price: 100 };
 let gloves = { name: "gloves", price: 75 };
-//additional property will be ignored by the compiler
+//hat, 100, Waterproof: undefined
+//gloves, 75, Waterproof: undefined
 let umbrella = { name: "umbrella", price: 30, waterproof: true };
 let products = [hat, gloves, umbrella];
-products.forEach(prod => console.log(`${prod.name}, ${prod.price}`));
+products.forEach(prod => console.log(`${prod.name}, ${prod.price}, Waterproof: ${prod.waterproof}`));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 let hat = {name: "Hat", price: 100};
 let gloves = {name: "gloves", price: 75};
 
-//additional property will be ignored by the compiler
+//hat, 100, Waterproof: undefined
+//gloves, 75, Waterproof: undefined
 let umbrella = {name: "umbrella", price: 30, waterproof: true};
 
-let products: {name: string, price: number}[]= [hat, gloves, umbrella];
+let products: {name: string, price: number, waterproof?: boolean }[]= [hat, gloves, umbrella];
 
-products.forEach(prod => console.log(`${prod.name}, ${prod.price}`));
+products.forEach(prod => console.log(`${prod.name}, ${prod.price}, Waterproof: ${prod.waterproof}`));
