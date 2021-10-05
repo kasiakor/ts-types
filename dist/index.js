09d71de08e46cba098e3ba3a53bb812aec2518f1
kasia.kordys <kasia.kordys@gmail.com>
2021-10-05 16:59:11 +0300

    use object shape type annotations

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,6 @@ let gloves = { name: "gloves", price: 75 };
 //property 'price' does not exist on type '{ name: string; }'
 //declaration changed to common properties let products: {name: string;}[];
 let umbrella = { name: "umbrella" };
+//property 'price' is missing in type '{ name: string; }' but required in type '{ name: string; price: number; }'
 let products = [hat, gloves, umbrella];
 products.forEach(prod => console.log(`${prod.name}, ${prod.price}`));
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ let gloves = {name: "gloves", price: 75};
 //declaration changed to common properties let products: {name: string;}[];
 let umbrella = {name: "umbrella"};
 
-
-let products = [hat, gloves, umbrella];
+//property 'price' is missing in type '{ name: string; }' but required in type '{ name: string; price: number; }'
+let products: {name: string, price: number}[]= [hat, gloves, umbrella];
 
 products.forEach(prod => console.log(`${prod.name}, ${prod.price}`));
