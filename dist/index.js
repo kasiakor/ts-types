5c9f9e26b5568de589a8abc720434bcd9e8167bc
kasia.kordys <kasia.kordys@gmail.com>
2021-10-12 14:11:56 +0300

    compiler error when infering types for subclasses

--- dist/index.js
+++ dist/index.js
@@ -22,10 +22,40 @@ class Employee extends Person {
         console.log(`Person ${this.name} works in ${this.dep}`);
     }
 }
-let data = [new Person("zarah", "Zara Howard", "Sidney"), new Employee("nickc", "Nick Cave", "PA", "Denver")];
+class Customer extends Person {
+    //constructor function
+    constructor(id, name, city, creditLimit) {
+        //superclass constructor is envoked
+        super(id, name, city);
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.creditLimit = creditLimit;
+    }
+}
+class Supplier extends Person {
+    //constructor function
+    constructor(id, name, city, companyName) {
+        //superclass constructor is envoked
+        super(id, name, city);
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.companyName = companyName;
+    }
+}
+let data = [new Employee("nickc", "Nick Cave", "PA", "Denver"), new Customer("zarah", "Zara Howard", "Sidney", 23000),];
+//argument of type 'Supplier' is not assignable to parameter of type 'Employee | Customer'.
+data.push(new Supplier("tomk", "Tom Kim", "London", "bbc"));
 data.forEach(item => {
     console.log(`Person's data: ${item.id}, ${item.name}, ${item.city}`);
     if (item instanceof Employee) {
         item.writeDep();
+    }
+    else if (item instanceof Customer) {
+        console.log(`Cutomer ${item.name} has this limit ${item.creditLimit}`);
+    }
+    else if (item instanceof Supplier) {
+        console.log(`Cutomer ${item.name} works for company called ${item.companyName}`);
     }
 });
--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,39 @@ class Employee extends Person {
     }
 }
 
-let data = [new Person("zarah", "Zara Howard", "Sidney"), new Employee("nickc", "Nick Cave", "PA", "Denver")]
+class Customer extends Person {
+    //constructor function
+    constructor( public readonly id: string, public name: string, public city: string, public creditLimit: number) {
+        //superclass constructor is envoked
+        super(id, name, city);
+    }
+}
+
+class Supplier extends Person {
+    //constructor function
+    constructor( public readonly id: string, public name: string, public city: string, public companyName: string) {
+        //superclass constructor is envoked
+        super(id, name, city);
+    }
+}
+
+let data = [new Employee("nickc", "Nick Cave", "PA", "Denver"), new Customer("zarah", "Zara Howard", "Sidney", 23000), ]
+
+
+//argument of type 'Supplier' is not assignable to parameter of type 'Employee | Customer'.
+data.push(new Supplier("tomk", "Tom Kim", "London", "bbc"));
 
 data.forEach(item => {
     console.log(`Person's data: ${item.id}, ${item.name}, ${item.city}`);
     if(item instanceof Employee) {
         item.writeDep();
+    }
+    else if(item instanceof Customer) {
+        console.log(`Cutomer ${item.name} has this limit ${item.creditLimit}`);
+    }
+
+    else if(item instanceof Supplier) {
+        console.log(`Cutomer ${item.name} works for company called ${item.companyName}`);
     }
 })
 
